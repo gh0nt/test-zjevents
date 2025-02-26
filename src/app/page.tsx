04c3b534126c0e96406baaa92a5b64c4c7c59bda
Ghont <giohan.puentes.hernandez@unillanos.edu.co>
2025-02-26 13:57:56 -0500

    fix: for imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import { Container } from "@/components/Container";
 import { Hero } from "@/components/Hero";
-import { SectionTitle } from "@/components/SectionTitle";
-import { Benefits } from "@/components/Benefits";
+
 import { Video } from "@/components/Video";
-import { Testimonials } from "@/components/Testimonials";
-import { Winners } from "@/components/Winners";
-import { Cta } from "@/components/Cta";
 
-//<Faq /> <Cta /> <Testimonials /> <Winners />
-
-import { benefitOne, benefitTwo } from "@/components/data";
 export default function Home() {
   return (
     <Container>
